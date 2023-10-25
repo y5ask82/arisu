3ed4c80436d2052b104c871022fbf3307ed1052c
Kyungwon Kim <gr1996@naver.com>
2023-10-25 11:47:53 +0900

    Scene UI 1
    씬 UI 1

---
+++ Assets/Scripts/GameQuit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameQuit : MonoBehaviour
+{
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+
+
+}
